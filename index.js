6e4425607cee203e124666f7afc2076dfbb2aad3
Blake Leonard <bleonard252@rocketmail.com>
2020-03-28 21:29:29 -0400

    Add some basic code [untested]

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const ARG = require('arg')
 const app = express()
+const DEBUG = require('debug')
+const debug = DEBUG("app")
 const xpware = require('./src') //the middleware is exposed from index.js
+
+//debug.log = console.info.bind(console);
+
 var args = ARG({
 	'--port': Number,
 	'--help': Boolean,
@@ -13,8 +18,11 @@ var args = ARG({
 const port = args.port || process.env.port || 3000
 
 //app.get('/', (req, res) => res.send('Hello World!'))
+//app.use(DEBUG("app:http"))
+
 app.use(xpware({})) //TODO: add config
 
-if (require.main === module) app.listen(port, () => console.log(`[index.js GOOD] listening on port ${port}`))
+if (require.main === module) app.listen(port, () => debug(`listening on port ${port}`))
 
-module.exports = xpware;
+module.exports = xpware; //require("myfview") will use the middleware.
+// so you can use app.use(require("myfview")({options...}))
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 var defaults = require('defaults');
 const fs = require('fs');
+const myf$debug = require('debug')('myfview');
+const express = require('express'); //typing
 
-module.exports = function (options) {
-    options = defaults(options || {},{
-        /** Path where the Myfiles are stored, sometimes myfview-config/myfiles/ */
-        "profilePath": __dirname+"/myfview-config/myfiles/",
+const defopts = {
+    /** Path where the Myfiles are stored, sometimes myfview-config/myfiles/ */
+    "profilePath": process.cwd()+"/myfview-config/myfiles/",
+    "configPath": process.cwd()+"/myfview-config/config.json",
+}
+
+module.exports = 
+    /**
+     * The Myfile Viewer (myfview) middleware. It gets myfiles from
+     * the filesystem (maybe databases later) and renders them in
+     * various formats, such as HTML, JSON, and text files.
+     * @param {defopts} options A set of options to pass to the middleware.
+     */
+    function myfview(options) {
+    options = defaults(options || {}, defopts); //look up
+    var config = JSON.parse(fs.readFileSync(options.configPath));
+    var configwatch = fs.watch(options.configPath, (ev) => { 
+        // Code to update the config when it's... updated
+        if (ev == "change") {
+            config = JSON.parse(fs.readFileSync(options.configPath))
+            myf$debug("Myfile config updated");
+        } else if (ev == "rename") {
+            myf$debug("WARNING: the config was moved or renamed! I'm not updating this file anymore!");
+            configwatch.close();
+        }
     });
     /**
      * Lookup a Myfile locally (i.e. on this server).
      * @param {string} name The Myfile username to look up.
-     * @returns {Promise} The Myfile data, or `null` if it doesn't exist.
+     * @returns {Promise<*>} The Myfile data (as a JSON object), or `null` if it doesn't exist.
      */
     function myf$lookup(name) {
         return Promise((resolve,reject) => {
@@ -18,13 +41,72 @@ module.exports = function (options) {
                     myf = JSON.parse(data.toString());
                     resolve(myf);
                 } else if (err.code == "ENOENT") resolve(null)
-                else reject(err)
+                else {
+                    myf$debug.extend('lookup()')("Error: %O",err)
+                    reject(err)
+                }
             })
         })
     }
-    return function (req, res, next) {
-        // Implement the middleware function based on the options object
-        next()
+    /**
+     * Get the style the myfile middleware should use
+     * to render the data. 
+     * 
+     * From `?type={type}` and `?{type}`.
+     * @param {string[]} q The query parameters.
+     * @param {string|null} t The `type` query parameter. Can be `null`.
+     * @param {string} u The `user-agent` header.
+     * @param {Function} a The `accept` function from Express.
+     * @returns {'html'|'cli'|'json'|'yaml'|'toml'} 
+     * The type of rendering to use, as a string.
+     * @default "json" 
+     * // If ?type exists and doesn't match a type or alias, it'll fall back to JSON instead.
+     */
+    function myf$getrentype(q,t,u,a) {
+        let types = {
+            "html": "html",
+            "htm": "html",
+            "web": "html",
+            "gui": "html",
+            "json": "json",
+            "js": "json",
+            "raw": "json",
+            "curl": "json", //TODO: change this to cli when it comes
+        }
+        if (t) return types[t] || "json" // if there's a type, use it or fall back
+        for (const p in q) { // if there's a type as a query parameter, use it!
+            if (q.hasOwnProperty(p)) { //this was in the snippet
+                if (types[p]) return types[p]
+            }
+        }
+        if (u.startsWith("curl/")) return types["curl"];
+        let acc = a("html, json");
+        /* */if (acc == "html") return "html";
+        else if (acc == "json") return "json";
+
+        // and if all else fails...
+        return "json"
     }
+    /**
+     * @param {express.Request} req @param {express.Response} res @param {express.NextFunction} next
+     */
+    function myf$middleware(req, res, next) {
+        if (req.path.startsWith("//")) {
+            req.myfuser = req.path.replace(/^\//,"");
+            req.rentype = myf$getrentype(req.params, req.query.type, req.header["user-agent"], req.header["accept"], req.accepts);
+            let user = req.myfuser;
+            let rt = req.rentype;
+            if (user != "") let prof = myf$lookup(user+"");
+            else let prof = myf$lookup(req.hostname); // i.e. google.com//google.com
+            if (rt == "html") {
+                //TODO: res.send(Handlebars...)
+            } else { // essentially: else if (rt == "json") {
+                res.type("json");
+                //TODO: config.json
+                res.send(prof)
+            }
+        } else next() // very important so that other things can happen
+    }
+    return myf$middleware
 }
 // vim: ts=8:sts=0:et:sw=4:smarttab
