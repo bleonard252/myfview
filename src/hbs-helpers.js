69e841f118358a133516c5143196894386ed4df9
Blake Leonard <bleonard252@rocketmail.com>
2020-03-30 15:06:37 -0400

    move more docs

--- src/hbs-helpers.js
+++ src/hbs-helpers.js
@@ -1,4 +1,6 @@
 const Handlebars = require('handlebars');
+const chalk = require('chalk');
+const ww = require('word-wrap');
 /**
  * myfview's Handlebars helpers.
  * 
@@ -10,32 +12,41 @@ const Handlebars = require('handlebars');
  * @param {globalThis.Handlebars} hbs Handlebars. This will get the helpers attached to it.
  */
 module.exports = function hbs$helpers(hbs) {
-    hbs.registerHelper("nlsp", 
     /** The handlebars helper `nlsp` (i.e. newline space).
-     * Useful to add indentation for the CLI mode. */
+     * Useful to add indentation for the CLI mode. 
+     * @param {int} spaces The number of spaces to indent each newline.
+     * @param {string} content What to apply the indentation to. 
+     * @public */
     function nlsp(spaces, content) {
         return content.replace("\n", "\n" + (" ".repeat(spaces)))
-    })
-    hbs.registerHelper("chalk", 
+    }
     /** The handlebars helper `chalk`.
-     * Useful to add colors to the CLI mode. */
+     * Useful to add colors to the CLI mode. 
+     * @param {string} cholor The color or formatting to apply.
+     * @param {string} content What to apply the formatting to.
+     * @public */
     function chalk(cholor, content) {
         try { return chalk[cholor](content) }
         catch (e) { myf$debug.extend("hbs:chalk")("Error: %O", e); return "" }
-    })
-    hbs.registerHelper("wrap", 
+    }
     /** The handlebars helper `wrap`.
      * Useful to limit the length of terminal
-     * lines in CLI mode. */
+     * lines in CLI mode. 
+     * @param {Number} width The maximum size for each line. 
+     * @param {string} str What to apply the wrapping to. 
+     * @public */
     function wrap(width, str) {
         return ww(str, { width, indent: `` })
-    })
-    hbs.registerHelper('cond', 
+    }
     /** The handlebars helper `cond`.
      * Improve the logic system.
-     * @example hbs
+     * ```handlebars
      * {{#if cond flag '||' otherflag}}
-     */
+     * ```
+     * @param {*} v1 One boolean value or condition.
+     * @param {'=='|'==='|'!='|'!=='|'<'|'<='|'>'|'>='|'&&'|'||'} operator The operation to apply.
+     * @param {*} v2 Another boolean value or condition.
+     * @public */
     function cond(v1, operator, v2) {
         switch (operator) {
             case '==':
@@ -61,6 +72,10 @@ module.exports = function hbs$helpers(hbs) {
             default:
                 return false
         }
-    });
+    }
+    hbs.registerHelper("nlsp", nlsp);
+    hbs.registerHelper("chalk", chalk);
+    hbs.registerHelper("wrap", wrap);
+    hbs.registerHelper("cond", cond);
     return hbs
 }
